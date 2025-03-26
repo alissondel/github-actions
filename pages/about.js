fbd2dd4ead4c9a8acf05227c716c439c875edd0a
Alisson Delatim <alisson.delatim@hotmail.com>
2025-03-26 20:58:28 -0300

    Update 26-03-2025

--- pages/about.js
+++ pages/about.js
@@ -2,6 +2,7 @@ export default function About() {
   return (
     <div>
       <h1>Page, about!</h1>
+      <p>wdwd</p>
     </div>
-  )
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -3,5 +3,5 @@ export default function Home() {
     <div>
       <h1>Page, Home!</h1>
     </div>
-  )
+  );
 }
