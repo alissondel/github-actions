1facd88ef73648af7338893ec2d2846d247f02a8
Alisson Delatim <alisson.delatim@hotmail.com>
2025-04-09 20:36:23 -0300

    Update Novo

--- scripts/pullRequestPreview.js
+++ scripts/pullRequestPreview.js
@@ -1,1 +1,8 @@
-console.log('Olá mundo!')
+import { execSync } from 'child_process';
+
+console.log('[DEPLOY_PREVIEW]: START');
+const command = "npm run deploy:staging";
+const output = execSync(command, { encoding: 'utf-8' });
+console.log('[DEPLOY_PREVIEW]: END');
+
+console.log("Output:" + output)
