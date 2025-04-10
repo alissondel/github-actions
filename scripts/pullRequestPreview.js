768aa9277f43e937f29bc35208e993588e5ba636
Alisson Delatim <alisson.delatim@hotmail.com>
2025-04-09 21:28:08 -0300

    E agora vaiii

--- scripts/pullRequestPreview.js
+++ scripts/pullRequestPreview.js
@@ -1,8 +1,8 @@
-import { execSync } from 'child_process';
+import { execSync } from 'child_process'
 
-console.log('[DEPLOY_PREVIEW]: START');
-const command = "npm run deploy:staging";
-const output = execSync(command, { encoding: 'utf-8' });
-console.log('[DEPLOY_PREVIEW]: END');
+console.log('[DEPLOY_PREVIEW]: START')
+const command = "npm run deploy:staging"
+const output = execSync(command, { encoding: 'utf-8' })
+console.log('[DEPLOY_PREVIEW]: END')
 
 console.log("Output:" + output)
